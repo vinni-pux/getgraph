7431ceb1f25b48456dded564458265a4b134449c
Konstantin Vinnikov <kotyaravin@rambler.ru>
2014-08-20 22:48:52 +0400

    change bar.html and update app.css

--- app/js/app.js
+++ app/js/app.js
@@ -6,7 +6,7 @@ angular.module('remoteWork', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.
 	when("/vacancies", {templateUrl: "partials/vacancies.html", controller: "vacanciesController"}).
-	when("/graph/column", {templateUrl: "partials/column.html", controller: "vacancyController"}).
+	when("/graph/bar", {templateUrl: "partials/bar.html", controller: "vacancyController"}).
 	when("/graph/line", {templateUrl: "partials/line.html", controller: "vacancyController"}).
 	when("/graph/pie", {templateUrl: "partials/pie.html", controller: "vacancyController"}).
 	otherwise({redirectTo: '/vacancies'});
